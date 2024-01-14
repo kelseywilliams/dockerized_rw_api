6676eb1f45d4a114b5bb39ed9b482efedfa5b605
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-01-14 14:05:03 -0800

    increased wait time of db_seeder service

--- database/migrations/seed.sql
+++ database/migrations/seed.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `User`
 --
 
-DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `api_keys`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `api_keys` (
