9d81bcdc22e6a3fa85f2692efb99d39fb06cd4a9
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-01-14 22:37:31 -0800

    Revisions to yaml file

--- web/config.php
+++ web/config.php
@@ -2,7 +2,7 @@
     $config=array(
         'host'=>'127.0.0.1',
         'username'=>'root',
-        'password'=>null,
+        'password'=> '',
         'db'=>'api'
         );
     return $config;
